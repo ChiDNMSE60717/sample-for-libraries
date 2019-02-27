beca92111fb0a11b6cc57bbd956706a4a509f145
Doan Nguyen Minh Chi <doannguyenminhchi@gmail.com>
2019-02-28 00:06:56 +0700

    Sample code export excel file

---
+++ excel-utils/sample_exporter.js
@@ -0,0 +1,48 @@
+import { Exporter } from '@chidoan/excel-utils';
+
+const filename = 'CHIDOAN';
+const headers = {
+    username: {
+      text: 'USERNAME',
+      type: 'String',
+      width: 200
+    },
+    birth_year: {
+      text: 'BIRTH_YEAR',
+      type: 'Number'
+    },
+    joining_date: {
+      text: 'JOINING DATE',
+      type: 'String',
+      width: 150
+    }
+};
+
+
+var exporter = new Exporter(filename);
+exporter.addSheet(headers,
+    [
+      {
+        username: 'chidoan',
+        birth_year: 1992,
+        joining_date: '12/12/2018'
+      },
+      {
+        username: 'toan.nguyen',
+        birth_year: 1991,
+        joining_date: '12/12/2018'
+      },
+      {
+        username: 'phuc_dao',
+        birth_year: 1992,
+        joining_date: '12/12/2018'
+      }
+    ],
+    'sheet_1'
+);
+var result = exporter.exportExcel();
+if (result.success) {
+	console.log(`Exporte file is successful`);
+} else {
+	console.error(result.message);
+}
