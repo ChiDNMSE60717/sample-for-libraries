295cf0d2f597f414e880141e5d06375df1e0e896
CHI DOAN <doannguyenminhchi@gmail.com>
2020-05-26 15:19:38 +0700

    Upgrade to 3.1.0 version

--- excel-utils/sample_exporter.js
+++ excel-utils/sample_exporter.js
@@ -1,7 +1,7 @@
 import { Exporter } from '@chidoan/excel-utils';
 
 /**
- * VERSION: 3.0.0
+ * VERSION: 3.1.0
  * 
  * NOTE: With DateTime type, you don't require to provide "format" field (default is General Date) or use on of value in below enum
  * Enum: General Date, Long Date, Medium Date, Short Date, Long Time, or other format in Excel for DateTime 
@@ -19,27 +19,36 @@ const headers = {
       text: 'BIRTH_YEAR',
       type: 'Number'
     },
+    assets: {
+      text: 'ASSETS',
+      type: 'Number',
+      format: '#,##0'
+    },
     joining_date: {
       text: 'JOINING DATE',
       type: 'DateTime',
-      width: 150
+      width: 150,
+      format: '[$-F800]dddd\,\ mmmm\ dd\,\ yyyy'
     }
 };
 const data = [
   {
     username: 'chidoan',
     birth_year: 1992,
-    joining_date: '2018-12-12'
+    joining_date: '2018-12-12',
+    assets: 203655000
   },
   {
     username: 'toan.nguyen',
     birth_year: 1991,
-    joining_date: '2018-12-12'
+    joining_date: '2018-05-21',
+    assets: 15000000
   },
   {
     username: 'phuc_dao',
     birth_year: 1992,
-    joining_date: '2018-12-12'
+    joining_date: '2018-03-31',
+    assets: 10025654
   }
 ];
 
